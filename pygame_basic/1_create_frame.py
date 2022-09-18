240cabaa6b3967add77f8fb0ae6eede8a17229c4
Sungpyo <pwsusc10@naver.com>
2022-09-18 11:10:32 +0900

    python pygame basic practice

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,23 @@
+import pygame
+
+# 초기화
+pygame.init()
+
+# 화면 크기 설정
+screen_width = 480 # 가로
+screen_height = 640 # 세로
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("My game")
+
+# 이벤트 루프
+running = True # 게임이 진행중인가?
+while running:
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
+            running = False # 게임 진행중이 아님
+
+
+#프로그램 종료
+pygame.quit()
---
+++ pygame_basic/test.py
@@ -0,0 +1,3 @@
+import random
+
+print("{0}".format(int(random.random() * 11)))
